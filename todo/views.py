7b73054089efb6cbe7e1784bd163075ba8ebfd61
Younse Park <loshinpark@gmail.com>
2020-06-22 01:27:58 +0900

    added calendar and style.css

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import TodoItem
+import calendar
 
 def todoView(request):
     all_todo_items = TodoItem.objects.all()
     return render(request, 'todo.html', 
     {'all_items' : all_todo_items})
+
+def printCal(request):
+    cal = calendar.HTMLCalendar(calendar.SUNDAY)
+    str = cal.formatmonth(2020, 6)
+    return HttpResponse(str)
 
 def addTodo(request):
     new_item = TodoItem(content = request.POST['content'])
@@ -19,3 +25,4 @@ def deleteTodo(request, todo_id):
     item_to_delete = TodoItem.objects.get(id = todo_id)
     item_to_delete.delete()
     return HttpResponseRedirect('/todo/')
+
